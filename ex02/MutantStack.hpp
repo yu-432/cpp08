97f50b12fc7775f5455cd1b5e8000dd7b4acf07d
yooshima <o.shimayu891@gmail.com>
2025-02-07 16:36:24 +0900

    :white_check_mark: add ex02

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: yooshima <yooshima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 15:05:32 by yooshima          #+#    #+#             */
-/*   Updated: 2025/02/06 15:37:52 by yooshima         ###   ########.fr       */
+/*   Updated: 2025/02/07 16:36:12 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,22 @@
 #include <stack>
 
 template<typename T>
-class MutantStack : public stack<T> {
-  
+class MutantStack : public std::stack<T> {
+  public:
+    MutantStack() : std::stack<T>() {};
+    MutantStack(const MutantStack& src) : std::stack<T>(src) {};
+    ~MutantStack() {};
+    
+    MutantStack& operator=(const MutantStack& src) {
+      if (this != &src) {
+        std::stack<T>::operator=(src);
+      };
+      return *this;
+    };
+    
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    iterator begin() { return this->c.begin(); };
+    iterator end() { return this->c.end(); };
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,57 +6,56 @@
 /*   By: yooshima <yooshima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 15:05:30 by yooshima          #+#    #+#             */
-/*   Updated: 2025/02/06 15:32:35 by yooshima         ###   ########.fr       */
+/*   Updated: 2025/02/07 16:35:32 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-// int main() {
-//   MutantStack<int> mstack;
-//   mstack.push(5);
-//   mstack.push(17);
-//   std::cout << mstack.top() << std::endl;
-//   mstack.pop();
-//   std::cout << mstack.size() << std::endl;
-//   mstack.push(3);
-//   mstack.push(5);
-//   mstack.push(737);
-//   //[...]
-//   mstack.push(0);
-//   MutantStack<int>::iterator it = mstack.begin();
-//   MutantStack<int>::iterator ite = mstack.end();
-//   ++it;
-//   --it;
-//   while (it != ite) {
-//     std::cout << *it << std::endl;
-//     ++it;
-//   }
-//   std::stack<int> s(mstack);
-//   return 0;
-// }
-
 int main() {
-  std::cout << "-----list-----" << std::endl;
-  std::list<int> mstack;
-  mstack.push_back(5);
-  mstack.push_back(17);
-  std::cout << mstack.back() << std::endl;
-  mstack.pop_back();
+  std::cout << "-----stack-----" << std::endl;
+  MutantStack<int> mstack;
+  mstack.push(5);
+  mstack.push(17);
+  std::cout << mstack.top() << std::endl;
+  mstack.pop();
   std::cout << mstack.size() << std::endl;
-  mstack.push_back(3);
-  mstack.push_back(5);
-  mstack.push_back(737);
+  mstack.push(3);
+  mstack.push(5);
+  mstack.push(737);
   //[...]
-  mstack.push_back(0);
-  std::list<int>::iterator it = mstack.begin();
-  std::list<int>::iterator ite = mstack.end();
-  ++it;
-  --it;
-  while (it != ite) {
-    std::cout << *it << std::endl;
-    ++it;
+  mstack.push(0);
+  MutantStack<int>::iterator it1 = mstack.begin();
+  MutantStack<int>::iterator ite1 = mstack.end();
+  ++it1;
+  --it1;
+  while (it1 != ite1) {
+    std::cout << *it1 << std::endl;
+    ++it1;
   }
-  std::list<int> s(mstack);
+  std::stack<int> s1(mstack);
+
+  
+  std::cout << "-----list-----" << std::endl;
+  std::list<int> mlist;
+  mlist.push_back(5);
+  mlist.push_back(17);
+  std::cout << mlist.back() << std::endl;
+  mlist.pop_back();
+  std::cout << mlist.size() << std::endl;
+  mlist.push_back(3);
+  mlist.push_back(5);
+  mlist.push_back(737);
+  //[...]
+  mlist.push_back(0);
+  std::list<int>::iterator it2 = mlist.begin();
+  std::list<int>::iterator ite2 = mlist.end();
+  ++it2;
+  --it2;
+  while (it2 != ite2) {
+    std::cout << *it2 << std::endl;
+    ++it2;
+  }
+  std::list<int> s2(mlist);
   return 0;
 }
